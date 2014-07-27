97cab93656eef3ca9201785b20d14ce475537afe
Ryota Arai <ryota.arai@gmail.com>
2014-07-27 22:46:13 +0900

    Remove copy_file method monkey patch. Because Specinfra has copy_file method now.

--- lib/lightchef.rb
+++ lib/lightchef.rb
@@ -3,7 +3,6 @@ require "lightchef/runner"
 require "lightchef/cli"
 require "lightchef/recipe"
 require "lightchef/resources"
-require "lightchef/monkey_patch"
 require "lightchef/logger"
 require "lightchef/node"
 
