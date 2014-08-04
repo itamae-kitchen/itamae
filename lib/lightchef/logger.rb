f746c4e65ab5332bb2764ba6eb5ae061d0502893
Ryota Arai <ryota.arai@gmail.com>
2014-08-04 12:20:58 +0900

    Change formatter of a logger.

--- lib/lightchef/logger.rb
+++ lib/lightchef/logger.rb
@@ -3,8 +3,33 @@ require 'logger'
 
 module Lightchef
   module Logger
+    class Formatter
+      def call(severity, datetime, progname, msg)
+        "[%s] %5s : %s\n" % [format_datetime(datetime), severity, msg2str(msg)]
+      end
+
+      private
+      def format_datetime(time)
+        time.strftime("%Y-%m-%dT%H:%M:%S.") << "%06d" % time.usec
+      end
+
+      def msg2str(msg)
+        case msg
+        when ::String
+          msg
+        when ::Exception
+          "#{ msg.message } (#{ msg.class })\n" <<
+          (msg.backtrace || []).join("\n")
+        else
+          msg.inspect
+        end
+      end
+    end
+
     def self.logger
-      @logger ||= ::Logger.new($stdout)
+      @logger ||= ::Logger.new($stdout).tap do |logger|
+        logger.formatter = Formatter.new
+      end
     end
 
     def self.logger=(l)
