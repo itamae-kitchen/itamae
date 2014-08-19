74c40879b5c41668b4c42da55fd77e37cc6abb09
Ryota Arai <ryota.arai@gmail.com>
2014-08-19 19:27:28 +0900

    Improve showing difference of directory resource.

--- lib/itamae/resource/directory.rb
+++ lib/itamae/resource/directory.rb
@@ -10,17 +10,26 @@ module Itamae
       define_attribute :group, type: String
 
       def set_current_attributes
-        if run_specinfra(:check_file_is_directory, path)
+        exist = run_specinfra(:check_file_is_directory, path)
+        @current_attributes[:exist?] = exist
+
+        if exist
           @current_attributes[:mode] = run_specinfra(:get_file_mode, path).stdout.chomp
           @current_attributes[:owner] = run_specinfra(:get_file_owner_user, path).stdout.chomp
           @current_attributes[:group] = run_specinfra(:get_file_owner_group, path).stdout.chomp
         else
-          updated!
+          @current_attributes[:mode] = nil
+          @current_attributes[:owner] = nil
+          @current_attributes[:group] = nil
+        end
+
+        if action == :create
+          @attributes[:exist?] = true
         end
       end
 
       def create_action
-        if ! run_specinfra(:check_file_is_directory, path)
+        if !run_specinfra(:check_file_is_directory, path)
           run_specinfra(:create_file_as_directory, path)
         end
         if attributes[:mode]
