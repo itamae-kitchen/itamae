4086329abd941bd3fe374476ab6aae6b4ce8be9d
Ryota Arai <ryota.arai@gmail.com>
2014-08-20 17:17:29 +0900

    Add tests for multiple actions.

--- spec/integration/recipes/default.rb
+++ spec/integration/recipes/default.rb
@@ -89,6 +89,24 @@ execute "ps -C cron > /tmp/cron_running; true"
 
 ######
 
+package "nginx"
+
+service "nginx" do
+  action [:enable, :start]
+end
+
+execute "test -f /etc/rc3.d/S20nginx" # test
+execute "test $(ps h -C nginx | wc -l) -gt 0" # test
+
+service "nginx" do
+  action [:disable, :stop]
+end
+
+execute "test ! -f /etc/rc3.d/S20nginx" # test
+execute "test $(ps h -C nginx | wc -l) -eq 0" # test
+
+######
+
 link "/tmp-link" do
   to "/tmp"
 end
