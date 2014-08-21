339f7ff5a5e8a8975ae1fb247a031cc62a77ccac
Ryota Arai <ryota.arai@gmail.com>
2014-08-21 14:31:17 +0900

    Implement Node#reverse_merge{,!}.

--- lib/itamae/node.rb
+++ lib/itamae/node.rb
@@ -4,6 +4,13 @@ require 'json'
 
 module Itamae
   class Node < Hashie::Mash
+    def reverse_merge(other_hash)
+      Hashie::Mash.new(other_hash).merge(self)
+    end
+
+    def reverse_merge!(other_hash)
+      self.replace(reverse_merge(other_hash))
+    end
   end
 end
 
--- spec/unit/lib/itamae/node_spec.rb
+++ spec/unit/lib/itamae/node_spec.rb
@@ -2,5 +2,12 @@ require 'spec_helper'
 
 module Itamae
   describe Node do
+    describe "#reverse_merge" do
+      it "merges a hash but the method receiver's value will be preferred" do
+        a = described_class.new(a: :b, c: :d)
+        expected = described_class.new(a: :b, c: :d, e: :f)
+        expect(a.reverse_merge(a: :c, e: :f)).to eq(expected)
+      end
+    end
   end
 end
