9e5da860b3766359b5143e9a69a58fabc305c3b8
Ryota Arai <ryota.arai@gmail.com>
2014-08-25 12:05:41 +0900

    Add TODO comments.

--- lib/itamae/resource/user.rb
+++ lib/itamae/resource/user.rb
@@ -15,6 +15,7 @@ module Itamae
         @current_attributes[:exist?] = exist?
 
         if @current_attributes[:exist?]
+          # TODO: delegate to Specinfra
           @current_attributes[:uid] = run_command(["id", "-u", username]).stdout.strip
           @current_attributes[:gid] = run_command(["id", "-g", username]).stdout.strip
           @current_attributes[:home] = run_command("echo ~#{shell_escape(username)}").stdout.strip
@@ -25,20 +26,24 @@ module Itamae
       def create_action
         if run_specinfra(:check_user_exists, username)
           if uid && uid.to_s != @current_attributes[:uid]
+            # TODO: delegate to Specinfra
             run_command(["usermod", "-u", uid, username]) 
             updated!
           end
 
           if gid && gid.to_s != @current_attributes[:gid]
+            # TODO: delegate to Specinfra
             run_command(["usermod", "-g", gid, username])
             updated!
           end
 
           if password && password != current_password
+            # TODO: delegate to Specinfra
             run_command("echo #{shell_escape("#{username}:#{password}")} | chpasswd -e")
             updated!
           end
         else
+          # TODO: delegate to Specinfra
           args = ["useradd"]
           args << "-g" << gid if gid
           args << "-d" << home if home
@@ -58,6 +63,7 @@ module Itamae
       end
 
       def current_password
+        # TODO: delegate to Specinfra
         result = run_command("cat /etc/shadow | grep -E ^#{shell_escape(username)}:", error: false)
 
         if result.exit_status == 0
