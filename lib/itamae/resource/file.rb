9fa6072c1aa8887b015ba77c6188b99535c589bb
Gosuke Miyashita <gosukenator@gmail.com>
2014-08-27 16:59:46 +0900

    Change how to handle tempfile
    Tempfile is removed when exit the block. So ocicasionally tempfile
    is removed before send_file method.
    
    So quit to use the block with tempfile and unlink file inside
    ensure block.

--- lib/itamae/resource/file.rb
+++ lib/itamae/resource/file.rb
@@ -12,17 +12,21 @@ module Itamae
       define_attribute :group, type: String
 
       def pre_action
-        src = if content_file
-                content_file
-              else
-                Tempfile.open('itamae') do |f|
+        begin
+          src = if content_file
+                  content_file
+                else
+                  f = Tempfile.open('itamae')
                   f.write(content)
+                  f.close
                   f.path
                 end
-              end
 
-        @temppath = ::File.join(runner.tmpdir, Time.now.to_f.to_s)
-        send_file(src, @temppath)
+          @temppath = ::File.join(runner.tmpdir, Time.now.to_f.to_s)
+          send_file(src, @temppath)
+        ensure
+          f.unlink
+        end
 
         case @current_action
         when :create
