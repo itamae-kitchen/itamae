5fecca0728d7e92b77195ec62685a62b52413be1
Gosuke Miyashita <gosukenator@gmail.com>
2014-08-28 12:12:29 +0900

    Load Itamae::Plugin::Resource::Foo if Itamae::Resource::Foo does not exist

--- lib/itamae/resource.rb
+++ lib/itamae/resource.rb
@@ -22,11 +22,23 @@ module Itamae
 
     class << self
       def get_resource_class_name(method)
-        method.to_s.split('_').map {|part| part.capitalize}.join
+        to_camel_case(method.to_s)
+      end
+
+      def get_resource_plugin_class_name(method)
+        '::Itamae::Plugin::Resource::' + to_camel_case(method.to_s)
+      end
+
+      def to_camel_case(str)
+        str.split('_').map {|part| part.capitalize}.join
       end
 
       def get_resource_class(method)
-        const_get(get_resource_class_name(method))
+        begin
+          const_get(get_resource_class_name(method))
+        rescue NameError => e
+          const_get(get_resource_plugin_class_name(method))
+        end
       end
 
       def parse_description(desc)
