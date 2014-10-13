8df4db880f1695d6c60fcd61944aa92443dfadfc
Kohei Suzuki <eagletmt@gmail.com>
2014-10-13 15:35:50 +0900

    Add cwd attribute to execute resource

--- lib/itamae/resource/execute.rb
+++ lib/itamae/resource/execute.rb
@@ -5,9 +5,10 @@ module Itamae
     class Execute < Base
       define_attribute :action, default: :run
       define_attribute :command, type: String, default_name: true
+      define_attribute :cwd, type: String
 
       def action_run(options)
-        run_command(attributes.command)
+        run_command(attributes.command, cwd: attributes.cwd)
         updated!
       end
     end
