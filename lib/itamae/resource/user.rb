29e088f534965ef2ce01b2ad0415b52cb691bc04
Ryota Arai <ryota.arai@gmail.com>
2015-01-27 16:23:42 +0900

    Accept group name as User's gid. Chef supports this too.

--- lib/itamae/resource/user.rb
+++ lib/itamae/resource/user.rb
@@ -5,7 +5,7 @@ module Itamae
     class User < Base
       define_attribute :action, default: :create
       define_attribute :username, type: String, default_name: true
-      define_attribute :gid, type: Integer
+      define_attribute :gid, type: [Integer, String]
       define_attribute :home, type: String
       define_attribute :password, type: String
       define_attribute :system_user, type: [TrueClass, FalseClass]
