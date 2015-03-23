0529e8592924d507fcf1e1fadb59d9f47e38d680
Ryota Arai <ryota.arai@gmail.com>
2015-03-23 19:28:41 +0900

    Fix Node class.

--- lib/itamae/node.rb
+++ lib/itamae/node.rb
@@ -4,36 +4,34 @@ require 'json'
 require 'schash'
 
 module Itamae
-  class Node < Hashie::Mash
+  class Node
     ValidationError = Class.new(StandardError)
 
-    def initialize(initial_hash, backend = nil)
-      super(initial_hash)
+    attr_reader :mash
+
+    def initialize(hash, backend)
+      @mash = Hashie::Mash.new(hash)
       @backend = backend
     end
 
     def reverse_merge(other_hash)
-      Hashie::Mash.new(other_hash).merge(self)
+      self.class.new(_reverse_merge(other_hash), @backend)
     end
 
     def reverse_merge!(other_hash)
-      self.replace(reverse_merge(other_hash))
+      @mash.replace(_reverse_merge(other_hash))
     end
 
     def [](key)
-      val = super(key)
-      if val.nil?
-        begin
-          val = @backend.host_inventory[key]
-        rescue NotImplementedError, NameError
-          val = nil
-        end
+      if @mash.has_key?(key)
+        @mash[key]
+      else
+        fetch_inventory_value(key)
       end
-      val
     end
 
     def validate!(&block)
-      errors = Schash::Validator.new(&block).validate(self)
+      errors = Schash::Validator.new(&block).validate(@mash)
       unless errors.empty?
         errors.each do |error|
           Logger.error "'#{error.position.join('->')}' #{error.message}"
@@ -41,6 +39,31 @@ module Itamae
         raise ValidationError
       end
     end
+
+    private
+
+    def _reverse_merge(other_hash)
+      Hashie::Mash.new(other_hash).merge(@mash)
+    end
+
+    def method_missing(method, *args)
+      if @mash.respond_to?(method)
+        return @mash.public_send(method, *args)
+      elsif args.empty? && value = fetch_inventory_value(method)
+        return value
+      end
+
+      super
+    end
+
+    def respond_to?(method, priv = false)
+      @mash.respond_to?(method, priv) || super
+    end
+
+    def fetch_inventory_value(key)
+      @backend.host_inventory[key]
+    rescue NotImplementedError, NameError
+      nil
+    end
   end
 end
-
--- spec/unit/lib/itamae/node_spec.rb
+++ spec/unit/lib/itamae/node_spec.rb
@@ -7,7 +7,7 @@ module Itamae
       it "merges a hash but the method receiver's value will be preferred" do
         a = described_class.new({a: :b, c: :d}, backend)
         expected = described_class.new({a: :b, c: :d, e: :f}, backend)
-        expect(a.reverse_merge(a: :c, e: :f)).to eq(expected)
+        expect(a.reverse_merge(a: :c, e: :f).mash).to eq(expected.mash)
       end
     end
   end
