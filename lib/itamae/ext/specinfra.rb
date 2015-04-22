07fd041ddff8a4bbfc1a2ec14fb998c454e9a37f
Ryota Arai <ryota-arai@cookpad.com>
2015-04-22 22:13:17 +0900

    Add #receive_file to Specinfra backend.

--- lib/itamae/ext/specinfra.rb
+++ lib/itamae/ext/specinfra.rb
@@ -1,2 +1,33 @@
 # TODO: Send patches to Specinfra
 
+module Specinfra
+  module Backend
+    class Exec < Base
+      def receive_file(from, to = nil)
+        if to
+          FileUtils.cp(from, to)
+        else
+          ::File.read(from)
+        end
+      end
+    end
+
+    class Ssh < Exec
+      def receive_file(from, to = nil)
+        scp_download!(from, to)
+      end
+
+      private
+
+      def scp_download!(from, to, opt={})
+        if get_config(:scp).nil?
+          set_config(:scp, create_scp)
+        end
+
+        scp = get_config(:scp)
+        scp.download!(from, to, opt)
+      end
+    end
+  end
+end
+
