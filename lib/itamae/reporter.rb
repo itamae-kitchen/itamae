87dc1b5976b47cfd3caab71f25d28f42588955e5
Ryota Arai <ryota.arai@gmail.com>
2015-12-06 23:06:40 +0900

    Require reporter class if loading fails.

--- lib/itamae/reporter.rb
+++ lib/itamae/reporter.rb
@@ -4,8 +4,19 @@ require 'itamae/reporter/debug'
 module Itamae
   module Reporter
     def self.from_type(type)
+      first_time = true
+
       class_name = type.split('_').map(&:capitalize).join
       self.const_get(class_name)
+    rescue NameError
+      require "itamae/reporter/#{type}"
+
+      if first_time
+        first_time = false
+        retry
+      else
+        raise
+      end
     end
   end
 end
