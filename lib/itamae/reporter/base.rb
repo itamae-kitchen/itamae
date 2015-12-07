a482ccb61d8e091c17fe469dc805fce440c228f0
Ryota Arai <ryota.arai@gmail.com>
2015-12-07 17:19:51 +0900

    Store stack of recipes, resources and actions.

--- lib/itamae/reporter/base.rb
+++ lib/itamae/reporter/base.rb
@@ -3,10 +3,27 @@ module Itamae
     class Base
       def initialize(options)
         @options = options
+
+        @recipes = []
+        @resources = []
+        @actions = []
       end
 
       def event(type, payload = {})
-        raise NotImplementedError
+        case type
+        when :recipe_started
+          @recipes << payload
+        when :recipe_completed, :recipe_failed
+          @recipes.pop
+        when :resource_started
+          @resources << payload
+        when :resource_completed, :resource_failed
+          @resources.pop
+        when :action_started
+          @actions << payload
+        when :action_completed, :action_failed
+          @actions.pop
+        end
       end
     end
   end
--- lib/itamae/reporter/debug.rb
+++ lib/itamae/reporter/debug.rb
@@ -2,6 +2,7 @@ module Itamae
   module Reporter
     class Debug < Base
       def event(type, payload = {})
+        super
         Itamae.logger.info("EVENT:#{type} #{payload}")
       end
     end
