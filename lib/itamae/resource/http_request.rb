ad22a217ecba001cd9aabef290657a97e82af3f1
Akihiko Horiuchi <12ff5b8@gmail.com>
2015-12-09 06:44:34 +0900

    Use 'required: true' on url attribute

--- lib/itamae/resource/http_request.rb
+++ lib/itamae/resource/http_request.rb
@@ -7,13 +7,9 @@ module Itamae
       UrlNotFoundError = Class.new(StandardError)
 
       define_attribute :headers, type: Hash, default: {}
-      define_attribute :url, type: String
+      define_attribute :url, type: String, required: true
 
       def pre_action
-        unless attributes.url
-          raise UrlNotFoundError, "url is not found"
-        end
-
         attributes.content = open(attributes.url, attributes.headers).read
 
         super
