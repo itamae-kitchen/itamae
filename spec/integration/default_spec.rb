2d00af5ebb2d6fb9d4023251d270c56479ce3b74
Yusuke Nakamura <yusuke1994525@gmail.com>
2018-01-29 08:57:34 +0900

    Ruby 2.3.1 includes test-unit as default gem

--- spec/integration/default_spec.rb
+++ spec/integration/default_spec.rb
@@ -199,10 +199,6 @@ describe command('gem list') do
   its(:stdout) { should include('rake (11.1.0') } # bundler 1.16 require rake 10
 end
 
-describe command('gem list') do
-  its(:stdout) { should_not include('test-unit') }
-end
-
 describe command('ri Bundler') do
   its(:stderr) { should eq("Nothing known about Bundler\n") }
 end
