94b539e8ac572e900819cbc8f74853aadbb376ad
sue445 <sue445@sue445.net>
2018-05-29 02:21:26 +0900

    Run serverspec using Docker backend

--- spec/integration/spec_helper.rb
+++ spec/integration/spec_helper.rb
@@ -1,36 +1,10 @@
-require 'serverspec'
-require 'net/ssh'
-require 'tempfile'
+require "serverspec"
+require "docker"
 
-set :backend, :ssh
+set :backend, :docker
 
-def vagrant(cmd)
-  env = {"VAGRANT_CWD" => File.dirname(__FILE__)}
-  system(env, "vagrant #{cmd}")
-end
-
-if ENV['ASK_SUDO_PASSWORD']
-  begin
-    require 'highline/import'
-  rescue LoadError
-    fail "highline is not available. Try installing it."
-  end
-  set :sudo_password, ask("Enter sudo password: ") { |q| q.echo = false }
-else
-  set :sudo_password, ENV['SUDO_PASSWORD']
-end
-
-host = ENV['TARGET_HOST']
-
-config = Tempfile.new('', Dir.tmpdir)
-vagrant "ssh-config #{host} > #{config.path}"
-
-options = Net::SSH::Config.for(host, [config.path])
-
-options[:user] ||= Etc.getlogin
-
-set :host,        options[:host_name] || host
-set :ssh_options, options
+set :docker_image, ENV["DOCKER_IMAGE"]
+set :docker_container, ENV["DOCKER_CONTAINER"]
 
 # Disable sudo
 # set :disable_sudo, true
