d5d29b61e06434438dc1d51140ac3b987e4f993b
NHH <h.nedim@gmail.com>
2026-03-04 11:49:26 +0100

    Move the log level validation tests

--- spec/unit/lib/itamae/logger_spec.rb
+++ spec/unit/lib/itamae/logger_spec.rb
@@ -40,5 +40,25 @@ module Itamae
         end
       end
     end
+
+    describe "::LEVELS" do
+      it "lists known log level names" do
+        expect(Itamae::Logger::LEVELS).to eq(%w[DEBUG INFO WARN ERROR FATAL UNKNOWN])
+      end
+    end
+
+    describe "#acceptable_level?" do
+      it "accepts valid log levels" do
+        %w[debug INFO Warn Error FATAL unknown].each do |level|
+          expect(Itamae.logger.acceptable_level?(level)).to be(true), "expected '#{level}' to be acceptable"
+        end
+      end
+
+      it "rejects invalid log levels" do
+        %w[FOO bar VERBOSE trace].each do |level|
+          expect(Itamae.logger.acceptable_level?(level)).to be(false), "expected '#{level}' to be rejected"
+        end
+      end
+    end
   end
 end
